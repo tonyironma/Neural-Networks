d33eda2902c89847981e036b5b3d895b33011eaa
tonyironma <112324464+tonyironma@users.noreply.github.com>
2023-04-15 18:50:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+Webcam.set({
+    width: 350,
+    height: 350,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach(' #camera' )
+
+function take_snapshot()
+{
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id= "captured_image" src ="'+data_uri+'"/>';
+});
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/E9QQzoVti/model.json',modelLoaded)
+
+function modelLoaded(){
+console.log("modelLoaded!");
+}
+
+function speak() {
+var synth = window.speechSynthesis;
+speak_data_1 = "The First Prediction is" + prediction_1
+speak_data_2 = "The Second Prediction is" + prediction_2
+var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img ,gotResult)
+}
+
+function gotResult(error, result){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(result)
+        document.getElementById('result_emotion_name').innerHTML = result[0].label;
+        document.getElementById('result_emotion_name2').innerHTML = result[1].label;
+        prediction_1 = result[0].label
+        prediction_2 = result[1].label
+    speak();
+    
+    if(result[0].label = "victory"){
+        document.getElementById("update_emogi").innerHTML = "&#9996;"
+    }
+
+    if(result[0].label = "amazing"){
+        document.getElementById("update_emogi").innerHTML = "&#128076;"
+    }
+
+    if(result[0].label = "best"){
+        document.getElementById("update_emogi").innerHTML = "&#128077;"
+    }
+
+    if(result[1].label = "victory"){
+        document.getElementById("update_emogi").innerHTML = "&#9996;"
+    }
+
+    if(result[1].label = "amazing"){
+        document.getElementById("update_emogi").innerHTML = "&#128076;"
+    }
+
+    if(result[1].label = "best"){
+        document.getElementById("update_emogi").innerHTML = "&#128077;"
+    }
+
+
+    }
+}
